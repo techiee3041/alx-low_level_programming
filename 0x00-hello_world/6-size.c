5b79f4e14e9b21f855bc0ea912fd7451c16f6da5
techiee3041 <doreenoms6699@gmail.com>
2023-02-16 07:13:31 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various variables
+ * Return: Return (0) success
+ */
+int main(void)
+{
+	printf("size of a char: %1d byte(S)\n", sizeof(char));
+	printf("size of an int: %1d byt(S)\n", sizeof(int));
+	printf("size of a long int: %1d byte(s)\n", sizeof(long int);
+	printf("size of a long long int: %1d byte(s)\n", sizeof(long long int);
+	printf("size of float: %1d byte(s)\n", sizeof(float);
+	return (0);
+}
