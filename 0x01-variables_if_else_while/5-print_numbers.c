a320c2e7f313b5b8209eebd45762bab568e7a6bb
techiee3041 <doreenoms6699@gmail.com>
2023-02-17 05:08:18 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
