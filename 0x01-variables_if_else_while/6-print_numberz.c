cb440982aff3de87a087f22443db2fc7bca71440
techiee3041 <doreenoms6699@gmail.com>
2023-02-17 05:28:23 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+	putchar('\n');
+	return (0);
+}
