9b87003e77894386dd55e3e169d8056c482672fb
techiee3041 <doreenoms6699@gmail.com>
2023-02-17 06:58:51 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
