aa01f2f213e30eebf93d8ba20e74618bd1a52e4c
techiee3041 <doreenoms6699@gmail.com>
2023-02-21 09:11:26 -0800

    change

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,13 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar('0');
 
-		for (j = 0; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
+
 			k = i * j;
 
 			if (k <= 9)
