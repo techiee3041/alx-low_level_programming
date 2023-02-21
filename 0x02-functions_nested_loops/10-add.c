cffdfa1fbfdaf05cac598fab144ead3371ed5359
techiee3041 <doreenoms6699@gmail.com>
2023-02-21 10:10:29 -0800

    change

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds values
+ * @x: value to be passed
+ * @y: number to be tested
+ * Return: returns the addition of two integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
