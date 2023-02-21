0bda95246bc61ac94b0c12fafbf44f2a4b528f51
techiee3041 <doreenoms6699@gmail.com>
2023-02-21 02:10:29 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets a to z
+ */
+
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * main - calls the function pritn_alphabet
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
