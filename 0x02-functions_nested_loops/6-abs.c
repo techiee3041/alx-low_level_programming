367e74f0fe1b88377075fee5add5586a50e89e6f
techiee3041 <doreenoms6699@gmail.com>
2023-02-21 06:05:53 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _abs - computes the value of an integer
+ * @int: the value being tested
+ * Return: when int < 0, returns int*-1, otherwise returns the value of integer
+ */
+
+
+int _abs(int)
+{
+	int x
+
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
