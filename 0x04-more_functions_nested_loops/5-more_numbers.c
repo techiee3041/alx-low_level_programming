a8125942e2952ef6ce1a2d5a13b44749de0cb047
techiee3041 <doreenoms6699@gmail.com>
2023-02-23 03:46:50 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 and 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
