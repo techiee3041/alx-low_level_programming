f3d8b17c2075a28d40116e4d5daa7a7d131fc8e2
techiee3041 <doreenoms6699@gmail.com>
2023-03-16 07:53:59 -0700

    created function allocating memory to malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,7 @@
 /**
  * *malloc_checked -- function that allocates memory to malloc
  * @b: size to be allocated
+ * Return: returns pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
