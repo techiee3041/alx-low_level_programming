7850cf95a4af4dbce7989263138201122bd14b3e
techiee3041 <doreenoms6699@gmail.com>
2023-03-30 16:38:14 +0300

    added a new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+
+
+/**
+ * add_node - adds a node to the linked list
+ * @head: pointer to head of linked list to find the size of
+ * @str: string to insert into the node of the linked listd
+ * Return: number of nodes in the list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+	int length;
+	char *newstr;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	else
+	{tmp->str = strdup(str);
+	tmp->len = strlen(len);
+	tmp->next = *head;
+
+	*head = tmp;
+	}
+
+	return(*head);
+}
